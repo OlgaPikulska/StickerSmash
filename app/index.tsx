df88716e395627d848e5201b9e54f75c822b5dc7
Olga Pikulska <olga.pikulska.dev@gmail.com>
2025-03-29 21:42:55 +0100

    Add About Screen

---
+++ app/index.tsx
@@ -0,0 +1,32 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello Expo!</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About Screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#25292e",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#ffffff",
+  },
+});
