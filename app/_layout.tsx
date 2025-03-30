00eb66555be0a0448b46264872d4519d44e94b1d
Olga Pikulska <olga.pikulska.dev@gmail.com>
2025-03-30 16:28:32 +0200

    Add not-found screen, about screen and tabs navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
+import { LogBox } from "react-native";
+
+LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerTitle: "Sticker Smash" }} />
-      <Stack.Screen name="about" options={{ headerTitle: "About" }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" options={{}} />
     </Stack>
   );
 }
